79cc92873cd8d8b139e2664faf911bd6294e91eb
lardira <danilkefimov@gmail.com>
2022-11-06 16:32:33 +0300

    styling for search and create post form

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -2,13 +2,15 @@ import './styles/SearchBox.css';
 
 const SearchBox = ({ className, onSearchHandler, placeHolder, value }) => {
   return (
-    <input
-      placeholder={placeHolder}
-      type='search'
-      className={`${className} search-box`}
-      onChange={onSearchHandler}
-      value={value}
-    ></input>
+    <div className='search-box-container'>
+      <input
+        placeholder={placeHolder}
+        type='search'
+        className={`${className} search-box`}
+        onChange={onSearchHandler}
+        value={value}
+      ></input>
+    </div>
   );
 };
 
